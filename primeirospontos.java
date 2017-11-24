698bfd291251fcab1cce609ef4b04cd876d06280
francisc011 <32169035+francisc011@users.noreply.github.com>
2017-11-24 15:54:49 +0000

    Add files via upload

---
+++ primeirospontos.java
@@ -0,0 +1,20 @@
+package primeirospontos;
+
+import java.util.Scanner;
+
+public class primeirospontos {
+    public static void main(String[] args) {
+        Scanner l = new Scanner (System.in);
+        
+        int soma=0;
+        int num;
+        System.out.print("Qual o número a introduzir?");
+        num = l.nextInt();
+        for (int I=1 ; I<=num ; I++){
+          soma +=I;
+       
+        }
+        System.out.println("O total é "+soma);
+    }
+    
+}
