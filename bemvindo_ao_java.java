08e7e301d0bbbca20e0eadc6af93916b3a12313f
francisc011 <32169035+francisc011@users.noreply.github.com>
2017-11-24 15:02:41 +0000

    Add files via upload

---
+++ bemvindo_ao_java.java
@@ -0,0 +1,11 @@
+package bem_vindo_ao_java;
+public class bemvindo_ao_java {
+
+	public static void main(String[] args) {
+		
+	String mensagem= "hoje é sexta feira";
+    for (int I=1 ; I<=5 ; I++)
+    	System.out.println(mensagem);
+	}
+
+}
