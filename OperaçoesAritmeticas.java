7cc6e20e0ffbfe9f796f8eb3a9aaeff47a76f6fb
francisc011 <32169035+francisc011@users.noreply.github.com>
2017-11-30 16:41:40 +0000

    Add files via upload

---
+++ OperaçoesAritmeticas.java
@@ -0,0 +1,13 @@
+package PrimeiroProjeto;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.printf("%d\n", 4+3*(15-2));
+		System.out.printf("%f\n" , 40.0/9);
+		System.out.printf("%f\n" , 40.0/3);
+		
+	}
+
+	
+}
